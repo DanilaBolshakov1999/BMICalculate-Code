e42f83c12305384f0193dd6632699ed8debe285d
Danila Bolshakov <danila1999777@gmail.com>
2023-07-26 15:17:12 +0300

    made the design of the first screen

--- BMICalculate-Code/ViewController.swift
+++ BMICalculate-Code/ViewController.swift
@@ -28,9 +28,29 @@ class ViewController: UIViewController {
         return label
     }()
     
+    private lazy var calculateButton: UIButton = {
+        let calculate = UIButton(type: .system)
+        calculate.tintColor = .white
+        calculate.backgroundColor = UIColor(red: 0.45, green: 0.45, blue: 0.82, alpha: 1.0)
+        calculate.layer.cornerRadius = 10
+        calculate.titleLabel?.font = .systemFont(ofSize: 20)
+        calculate.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        calculate.translatesAutoresizingMaskIntoConstraints = false
+        return calculate
+    }()
     
     //MARK: - Stack
     private lazy var mainStackView = UIStackView()
+    
+    private lazy var heightStackView = UIStackView()
+    private lazy var heightTitleView = UILabel(textAlignment: .left)
+    private lazy var heightNumberView = UILabel(textAlignment: .right)
+    private lazy var heighSlider = UISlider(maxValue: 3)
+    
+    private lazy var weightStackView = UIStackView()
+    private lazy var weightTitleView = UILabel(textAlignment: .left)
+    private lazy var weightNumberView = UILabel(textAlignment: .right)
+    private lazy var weightSlider = UISlider(maxValue: 300)
     
     //MARK: - Life Cycle
     override func viewDidLoad() {
@@ -39,19 +59,50 @@ class ViewController: UIViewController {
         setupConstrains()
     }
     
+    //MARK: - setViews()
     private func setViews() {
-        mainStackView = UIStackView(
+        
+        heightStackView = UIStackView(
             axis: .horizontal,
+            distribution: .fillEqually,
+            subView: [heightTitleView, heightNumberView])
+        
+        weightStackView = UIStackView(
+            axis: .horizontal,
+            distribution: .fillEqually,
+            subView: [weightTitleView, weightNumberView])
+        
+        mainStackView = UIStackView(
+            axis: .vertical,
             distribution: .fillProportionally,
-            subView: [titleLabel]
-        )
+            subView: [
+                titleLabel,
+                heightStackView,
+                heighSlider,
+                weightStackView,
+                weightSlider,
+                calculateButton
+            ])
         
         view.addSubview(backgroundView)
         view.addSubview(mainStackView)
+        
         titleLabel.text = "CALCULATE YOU BMI"
-    
+        
+        heightTitleView.text = "Hight"
+        heightNumberView.text = "1.5 m"
+        
+        weightTitleView.text = "Wight"
+        weightNumberView.text = "100 kg"
+        
+        calculateButton.setTitle("CALCULATE", for: .normal)
+        
         
     }
+    @objc func buttonTapped() {
+        print("Tap-tap")
+    }
+    
 }
 
 
@@ -66,7 +117,15 @@ extension ViewController {
             mainStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
             mainStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
             mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            
+            heightStackView.heightAnchor.constraint(equalToConstant: 21),
+            heighSlider.heightAnchor.constraint(equalToConstant: 60),
+            
+            weightStackView.heightAnchor.constraint(equalToConstant: 21),
+            weightSlider.heightAnchor.constraint(equalToConstant: 60),
+            
+            calculateButton.heightAnchor.constraint(equalToConstant: 51)
         ])
     }
 }
@@ -81,3 +140,24 @@ extension UIStackView {
     }
 }
 
+extension UILabel {
+    convenience init(textAlignment: NSTextAlignment) {
+        self.init()
+        self.textAlignment = textAlignment
+        self.font = .systemFont(ofSize: 17, weight: .light)
+        self.text = text
+        self.textColor = textColor
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+extension UISlider {
+    convenience init(maxValue: Float) {
+        self.init()
+        self.maximumValue = 3
+        self.value = maxValue / 2
+        self.thumbTintColor = UIColor(red: 0.45, green: 0.45, blue: 0.82, alpha: 0.5)
+        self.minimumTrackTintColor = UIColor(red: 0.45, green: 0.45, blue: 0.82, alpha: 0.5)
+        self.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
